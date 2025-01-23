6e8449bb2a9f73c13a24fc11b7d4ce71468edcc6
CapellaAgente <yuri.a.capella@gmail.com>
2025-01-23 19:32:35 -0300

    host test

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t^#ya8o0$*^*isb-4tnhp*vo+qphc^%lth_$k^6am_v06d4ony
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
